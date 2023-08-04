ac3cfbc31e6e319b0c77309fa6c6d4b153902778
ayhem18 <bouabidayhem@gmail.com>
2023-08-04 18:49:52 +0100

    layer-level transfer with resnet: passed

--- src/pytorch_modular/transfer_learning/testing/test_layer_transfer.py
+++ src/pytorch_modular/transfer_learning/testing/test_layer_transfer.py
@@ -2,12 +2,32 @@
 This script contains functionality to test the forward pass of the FeatureExtractor class
 """
 import random
-
+import os
+import sys
 import numpy as np
 import torch
-import src.pytorch_modular.transfer_learning.transfer_resnet as tl_res
 import unittest
 
+# since the 'src' directory is a non-direct parent of the current file
+# it is not visible which might raise a ModuleNotFoundError
+
+# this small piece of code overcomes this
+
+from pathlib import Path
+
+try:
+    import src.pytorch_modular.transfer_learning.transfer_resnet as tl_res
+except ModuleNotFoundError:
+    h = os.getcwd()
+    while 'src' not in os.listdir(h):
+        print(h)
+        h = Path(h).parent
+    # at this point 'dir' should be the root of the repository
+    # make sure to convert the path to a string as sys.path does not recognize Path objects
+    sys.path.append(str(h))
+
+import src.pytorch_modular.transfer_learning.transfer_resnet as tl_res
+from src.pytorch_modular.pytorch_utilities import get_default_device
 from torch import nn
 from torchvision.models import resnet50, ResNet50_Weights
 from collections import OrderedDict
@@ -19,18 +39,23 @@ random.seed(69)
 class FeatureExtractorTest(nn.Module):
     """
     This class is a simple but non-expandable implementation of the featureExtractor class
-    extremly close outputs of both classes guarantees the correctness of FeatureExtractor class
+    Having close outputs for both classes guarantees the correctness of the FeatureExtractor class\
     """
 
     def __init__(self, num_layers: int,
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__net = resnet50(ResNet50_Weights.DEFAULT)
+        for p in self.__net.parameters():
+            p.requires_grad = False
         self.num_layers = num_layers
         self.layers_dict = OrderedDict([(1, self.__net.layer1),
                                         (2, self.__net.layer2),
                                         (3, self.__net.layer3),
                                         (4, self.__net.layer4)])
+        self.modules = [self.__net.conv1, self.__net.bn1, self.__net.relu, self.__net.maxpool]
+        self.modules.extend([self.layers_dict[i] for i in range(1, min(self.num_layers, 4) + 1)])
+        self.modules.append(self.__net.avgpool)
 
     def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
         # this function is basically a copy of the ResNet50._forward_impl function
@@ -43,41 +68,50 @@ class FeatureExtractorTest(nn.Module):
 
         for i in range(1, min(self.num_layers, 4) + 1):
             x = self.layers_dict[i].forward(x)
+        # make sure to pass through the average pooling layer at the end
+        x = self.__net.avgpool(x)
         return x
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        self._forward_impl(x)
+        return self._forward_impl(x)
 
 
 class TestFeatureExtractorOutput(unittest.TestCase):
-    def __init__(self, num_inner_tests: int = 1000):
-        super().__init__()
-        self.tests = num_inner_tests
-
     def test_total_output(self):
+        # calling this function will
+        device = get_default_device()
+        print(device)
+
         layers = list(range(1, 5))
         # add a random number larger 4 to make sure the FeatureExtractor code doesn't break
         layers.append(random.randint(a=5, b=10))
+
         for v in layers:
             # first create the needed modules
             test_fe = FeatureExtractorTest(num_layers=v)
             tested_fe = tl_res.FeatureExtractor(blocks_to_keep=v)
+
+            tested_fe.to(device)
+            test_fe.to(device)
+
             # make sure to set both modules to test modes
             test_fe.eval()
             tested_fe.eval()
 
-            for _ in range(self.tests):
+            for _ in range(1000):
                 batch, height, width = random.randint(a=4, b=12), \
                     random.randint(a=244, b=512), \
                     random.randint(a=244, b=512)
 
                 # torch.conv2d only works with 3 channel input
-                x = torch.rand(batch, 3, height, width)
-                true_output = test_fe(x).detach().cpu().numpy()
-                test_output = tested_fe(x).detach().cpu().numpy()
+                x = torch.rand(batch, 3, height, width).to(device)
+                true_output = test_fe(x).cpu().numpy()
+                test_output = tested_fe(x).cpu().numpy()
+
                 correct = np.allclose(true_output, test_output, equal_nan=True)
-                self.assertTrue(correct)
+                self.assertTrue(correct, 'The outputs of both classes are too different')
 
 
 if __name__ == '__main__':
     unittest.main()
+
--- src/pytorch_modular/transfer_learning/transfer_resnet.py
+++ src/pytorch_modular/transfer_learning/transfer_resnet.py
@@ -4,6 +4,7 @@ This script contains functionalities to build classifiers on top of the pretrain
 
 This script is mainly inspired by this paper: 'https://arxiv.org/abs/1411.1792'
 as they suggest an experimental framework to find the most transferable / general layers
+in pretrained network. I am applying the same framework on the resnet architecture.
 """
 import os
 import sys
@@ -19,7 +20,6 @@ sys.path.append(HOME)
 sys.path.append(os.path.join(HOME, 'src'))
 
 # from torchvision.models.resnet50 import
-
 LAYER_BLOCK = 'layer'
 RESIDUAL_BLOCK = 'residual'
 
@@ -40,7 +40,7 @@ class FeatureExtractor(nn.Module):
     # a function to build the transferred part from the original resnet model
     # in case of 'layer' blocks
 
-    def __feature_extractor_layers(self, number_of_layers: int, freeze: bool):
+    def __feature_extractor_layers(self, number_of_layers: int):
         modules_generator = self.__net.named_children()
         # the modules will be saved with their original names in an OrderedDict and later merged
         # into a single nn.Module using nn.Sequential
@@ -55,31 +55,28 @@ class FeatureExtractor(nn.Module):
                     # only increment counter if the name contains 'layer': meaning it is a layer block
                     counter += int('layer' in name)
 
+        self.modules = [m for n, m in modules_to_keep]
         fe = nn.Sequential(OrderedDict(modules_to_keep))
-        if freeze:
-            for p in fe.parameters():
-                p.requires_grad = False
-
         return fe
 
     # a function to build the transferred part from the original resnet model
     # is case of 'residual' block
-    def __feature_extractor_residuals(self, number_of_blocks: int, freeze: bool):
-        modules_generator = self.__net.named_modules(recurse=False, remove_duplicate=False)
-        # first let's remove the first element
-        next(modules_generator)
+    def __feature_extractor_residuals(self, number_of_blocks: int):
+        modules_generator = self.__net.named_children()
         counter = 0
         modules_to_keep = []
         # this part is slightly more complex
         for name, module in modules_generator:
             # discard any fully connected components of the network
             if not contains_fc_layer(module):
+                # the residual blocks lie within the 'layer' blocks
                 if 'layer' in name:
                     # here we will build a nn.Sequential object that uses as many residual blocks as possible
                     new_layer = []
-                    for _, sub_module in module.name_modules(recurse=False, remove_duplicate=False):
+                    for _, sub_module in module.named_children():
                         if not isinstance(sub_module, Bottleneck) or counter < number_of_blocks:
                             new_layer.append(sub_module)
+                            # only increment the counter is the sub_module added is
                             counter += int(isinstance(sub_module, BottleNeck))
                     # time to wrap this new layer in a nn.Sequential to use freely with the rest of the model
                     new_layer = nn.Sequential(new_layer)
@@ -87,12 +84,8 @@ class FeatureExtractor(nn.Module):
                 else:
                     modules_to_keep.append(module)
 
-        modules_to_keep = [m for m in next_module()]
+        self.modules = [m for n, m in modules_to_keep]
         fe = nn.Sequential(modules_to_keep)
-        if freeze:
-            for p in fe.parameters():
-                p.requires_grad = False
-
         return fe
 
     def __init__(self, blocks_to_keep: int,  # the number of blocks to keep
@@ -105,18 +98,21 @@ class FeatureExtractor(nn.Module):
 
         self.num_blocks = blocks_to_keep
         # make sure to have
-        self.__net = resnet50()
-        self.feature_extractor = None
+        self.__net = resnet50(ResNet50_Weights.DEFAULT)
+        # before proceeding to extract the feature set freeze the layer if needed
+        if freeze:
+            for para in self.__net.parameters():
+                para.requires_grad = False
 
-        self.feature_extractor = self.__feature_extractor_layers(blocks_to_keep, freeze=freeze) \
-            if blocks_type == LAYER_BLOCK else self.__feature_extractor_residuals(blocks_to_keep, freeze=freeze)
+        self.feature_extractor = None
+        self.modules = None
+        self.feature_extractor = self.__feature_extractor_layers(blocks_to_keep) \
+            if blocks_type == LAYER_BLOCK else self.__feature_extractor_residuals(blocks_to_keep)
 
     def forward(self, x: torch.Tensor):
         # the forward function in the ResNet class simply calls the forward function
-        # of each submodule consecutively: which is equivalent of passing these values
-        # after inspecting the forward logic in the source code:
-        # https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
-        # we can see
+        # of each submodule consecutively: which is equivalent to saving all modules in a nn.Sequential module
+        # and calling the forward function of the component.
         return self.feature_extractor.forward(x)
 
     def __str__(self):
@@ -129,6 +125,4 @@ if __name__ == '__main__':
     default_weights = ResNet50_Weights.DEFAULT
     net = resnet50(default_weights)
     # let's build a feature extractor
-    fe = FeatureExtractor(2)
-    output = fe.forward(torch.rand(1, 3, 244, 244))
-    print(torch.squeeze(output))
+    print(net)
